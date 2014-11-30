eb5112e5fb5e18392934e46aa8d71c180e35a627
matelich <thomas@mateli.ch>
2014-11-29 16:15:34 -0800

    fix plural of minute, add mirror cool time check, add test mode, make bg partially transparent

--- js/cooltime.js
+++ js/cooltime.js
@@ -19,6 +19,34 @@ $(document).ready(function() {
 
 	update();
 	window.setTimeout(function() { update(); window.setInterval(update, 60*1000); }, 61*1000-d.getSeconds()*1000-d.getMilliseconds())
+
+	if(window.location.search) {
+		var testoutput = document.getElementById("test");
+		testoutput.innerHTML = "Test Results<br/>";
+		var test = new Date();
+		test.setHours(1,0);
+		var coolinfo = { type: "unknown"};
+		for(var i = 0; i < 12*60; i++) {
+			var hours = test.getHours();
+			if(hours > 12) { hours = hours - 12; }
+			var timetext = hours + ':';
+			var min = test.getMinutes();
+			if(min < 10) { timetext += '0';}
+			timetext += min;
+
+			var cool = checkCoolness(test, coolinfo);
+			if(!cool) {
+				testoutput.innerHTML += timetext + " is not cool <br/>";
+			} else {
+				testoutput.innerHTML += timetext + " is cool! " + cleanOutput(coolinfo.type) + "<br/>";
+			}
+			if(test.getMinutes() != 59) {
+				test.setMinutes(test.getMinutes()+1);
+			}else{
+				test.setHours(test.getHours()+1, 0);
+			}
+		}
+	}
 });
 
 function update()
@@ -42,7 +70,13 @@ function update()
 				var min = d.getMinutes();
 				if(min < 10) { timetext += '0';}
 				timetext += min;
-				timeinfo.innerHTML = "<br/>The next cool time is "+timetext+" in "+time_til_coolness+" minutes!  " + cleanOutput(coolinfo.type);
+				var message =  "<br/>The next cool time is "+timetext+" in "+time_til_coolness;
+				if(time_til_coolness == 1)
+					message += " minute!  ";
+				else
+					message += " minutes!  ";
+				message += cleanOutput(coolinfo.type);
+				timeinfo.innerHTML = message;
 				break;
 			}
 		}
@@ -78,6 +112,14 @@ function checkCoolness(d, coolinfo)
 				coolinfo.type = "All of the digits are the same";
 				return true;
 			}
+		}
+	}
+
+	//check mirrored
+	if(ht != 0) {
+		if(ht == mo && ho == mt) {
+			coolinfo.type = "Hours and minutes are mirrored";
+			return true;
 		}
 	}
 
